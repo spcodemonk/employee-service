493681d301203811ccd67decf52d2f241d20cfea
TechSP <sp@example.com>
2021-09-25 20:56:06 +0530

    added dto to entity model mapper code changes

--- employee-service/src/main/java/com/sp/employeeservice/EmployeeController.java
+++ employee-service/src/main/java/com/sp/employeeservice/EmployeeController.java
@@ -1,10 +1,16 @@
 package com.sp.employeeservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sp.employeeservice.dto.EmployeeDto;
+import com.sp.employeeservice.model.Employee;
 import com.sp.employeeservice.service.EmployeeService;
 
 @RestController
@@ -14,10 +20,20 @@ public class EmployeeController {
 	@Autowired
 	EmployeeService empService;
 
+	@Autowired
+	private ModelMapper modelMapper;
+
 	@GetMapping("/allEmps")
 	public String FindAllEmployees() {
 
 		return "Hi Sharad";
+	}
+
+	@PostMapping("/save-emp")
+	public String saveEmployee(@RequestBody EmployeeDto empDto) {
+		Employee emp = modelMapper.map(empDto, Employee.class);
+		System.out.println(emp.toString());
+		return new String("Sharad");
 	}
 
 }
---
+++ employee-service/src/main/java/com/sp/employeeservice/dto/EmployeeDto.java
@@ -0,0 +1,12 @@
+package com.sp.employeeservice.dto;
+
+import com.sp.employeeservice.model.Employee;
+
+import lombok.Data;
+
+@Data
+public class EmployeeDto {
+	private int id;
+	private String name;
+
+}
